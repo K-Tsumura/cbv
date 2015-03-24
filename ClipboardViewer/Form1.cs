213b18cf36c19f850351b27c62b2c14e4850e159
ElecDove(homeDesktop) <jpp_elec_dove@ybb.ne.jp>
2015-03-24 15:02:53 +0900

    FirstCommit

---
+++ ClipboardViewer/Form1.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace ClipboardViewer
+{
+    public partial class Form1 : Form
+    {
+        private MyClipboardViewer viewer;
+
+        public Form1()
+        {
+            viewer = new MyClipboardViewer(this);
+
+            // イベントハンドラを登録
+            viewer.ClipboardHandler += this.OnClipBoardChanged;
+            InitializeComponent();
+        }
+
+        // クリップボードにテキストがコピーされると呼び出される
+        private void OnClipBoardChanged(object sender, ClipboardEventArgs args)
+        {
+
+            //Console.WriteLine("OnClipboardChanged:" + val.ToClipboardButton);
+
+            //改行、スペース、タブを半角スペースに置き換えて表示する
+            this.label1.Text = Regex.Replace(args.Text, "(　| |\t|\r\n)", "");
+
+            if (val.ToClipboardButton)
+            {
+                val.ToClipboardButton = false;
+                return;
+            }
+
+
+            for (int j = 0; j < val.moji.Length - 1; j++)
+            {
+                if (args.Text == val.moji[j])
+                {
+
+                    for (int k = j; k < val.moji.Length - 1; k++)
+                    {
+                        val.moji[k] = val.moji[k + 1];
+                    }
+
+                    break;
+                }
+            }
+            for (int i = val.moji.Length-1; i >= 1; i--)
+            {
+                val.moji[i] = val.moji[i - 1];
+            }
+            val.moji[0] = args.Text;
+            textBoxReDraw();
+        }
+
+        public void textBoxReDraw()
+        {
+            this.textBox1.Text = val.moji[0];
+            this.textBox2.Text = val.moji[1];
+            this.textBox3.Text = val.moji[2];
+            this.textBox4.Text = val.moji[3];
+            this.textBox5.Text = val.moji[4];
+            this.textBox6.Text = val.moji[5];
+            this.textBox7.Text = val.moji[6];
+            this.textBox8.Text = val.moji[7];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetDataObject(this.textBox1.Text, false);
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            val.ToClipboardButton = true;
+            Console.WriteLine(val.ToClipboardButton);
+            Clipboard.SetDataObject(this.textBox2.Text, false);
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            val.ToClipboardButton = true;
+            Clipboard.SetDataObject(this.textBox3.Text, false);
+            
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            val.ToClipboardButton = true;
+            Clipboard.SetDataObject(this.textBox4.Text, false);
+            
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            val.ToClipboardButton = true;
+            Clipboard.SetDataObject(this.textBox5.Text, false);
+            
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            val.ToClipboardButton = true;
+            Clipboard.SetDataObject(this.textBox6.Text, false);
+            
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            val.ToClipboardButton = true;
+            Clipboard.SetDataObject(this.textBox7.Text, false);
+            
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            val.ToClipboardButton = true;
+            Clipboard.SetDataObject(this.textBox8.Text, false);
+            
+        }
+
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.checkBox1.Checked)
+            {
+                TopMost = true;
+            }
+            else
+            {
+                TopMost = false;
+            }
+
+        }
+
+        private void 終了ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            notifyIcon1.Visible = false;
+            Application.Exit();
+        }
+        
+        
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.ShowInTaskbar = false;
+                this.notifyIcon1.Visible = true;
+            }
+            else
+            {
+                this.notifyIcon1.Visible = false;
+                this.ShowInTaskbar = true;
+            }
+
+        }
+
+        //通知領域アイコンをダブルクリックされたとき
+        //ウインドウをもとに戻す
+        private void NotifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+        }
+
+
+        private Point mouseposition;
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+            {
+                //位置を記憶する
+                mouseposition = new Point(e.X, e.Y);
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            {
+                if ((e.Button & MouseButtons.Left) == MouseButtons.Left)
+                {
+                    this.Left += e.X - mouseposition.X;
+                    this.Top += e.Y - mouseposition.Y;
+                }
+            }
+        }
+
+        private void コンパクトにするToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormBorderStyle = FormBorderStyle.None;
+            コンパクトにするToolStripMenuItem.Enabled = false;
+        }
+        private void 元の大きさに戻すToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            元の大きさに戻すToolStripMenuItem.Enabled = false;
+        }
+
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                //コンテキストメニューを表示する座標
+                System.Drawing.Point p = System.Windows.Forms.Cursor.Position;
+
+                //指定した画面上の座標位置にコンテキストメニューを表示する
+                this.contextMenuStrip1.Show(p);
+            }
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+            if (FormBorderStyle == FormBorderStyle.FixedSingle)
+            {
+                元の大きさに戻すToolStripMenuItem.Enabled = false;
+                コンパクトにするToolStripMenuItem.Enabled = true;
+            }
+            else
+            {
+                コンパクトにするToolStripMenuItem.Enabled = false;
+                元の大きさに戻すToolStripMenuItem.Enabled = true;
+            }
+        }
+
+        private void 終了ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            //終了する
+            終了ToolStripMenuItem_Click(sender, e);
+        }
+
+        private void label1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Form1_MouseClick(sender, e);
+        }
+
+
+
+
+        
+
+    }
+
+    public class val
+    {
+        public static string[] moji = new string[9];
+        public static bool ToClipboardButton = false;
+    }
+
+}
---
+++ ClipboardViewer/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Runtime.InteropServices;
+
+
+namespace ClipboardViewer
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            //フォームが閉じられるときは、現在クリップボードにあるテキストを
+            //クリップボードにコピーする。第二引数がtrueなのでこのウインドウを閉じても
+            //そのデータは保持されるようになる。
+            if (Clipboard.GetText() != "")
+            {
+                Clipboard.SetDataObject(Clipboard.GetText(), true);
+            }
+        }
+    }
+}
+
+
+namespace ClipboardViewer
+{
+  public class ClipboardEventArgs : EventArgs
+  {
+    private string text;
+
+    public string Text
+    {
+      get { return this.text; }
+    }
+
+    public ClipboardEventArgs(string str)
+    {
+      this.text = str;
+    }
+  }
+
+public delegate void cbEventHandler(
+                        object sender, ClipboardEventArgs ev);
+
+  [System.Security.Permissions.PermissionSet(
+        System.Security.Permissions.SecurityAction.Demand,
+        Name = "FullTrust")]
+  internal class MyClipboardViewer : NativeWindow
+  {
+    [DllImport("user32")]
+    public static extern IntPtr SetClipboardViewer(
+            IntPtr hWndNewViewer);
+
+    [DllImport("user32")]
+    public static extern bool ChangeClipboardChain(
+            IntPtr hWndRemove, IntPtr hWndNewNext);
+
+    [DllImport("user32")]
+    public extern static int SendMessage(
+            IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+
+    private const int WM_DRAWCLIPBOARD = 0x0308;
+    private const int WM_CHANGECBCHAIN = 0x030D;
+    private IntPtr nextHandle;
+
+    private Form parent;
+    public event cbEventHandler ClipboardHandler;
+
+    public MyClipboardViewer(Form f)
+    {
+      f.HandleCreated
+                  += new EventHandler(this.OnHandleCreated);
+      f.HandleDestroyed
+                  += new EventHandler(this.OnHandleDestroyed);
+      this.parent = f;
+    }
+
+    internal void OnHandleCreated(object sender, EventArgs e)
+    {
+      AssignHandle(((Form)sender).Handle);
+      // ビューアを登録
+      nextHandle = SetClipboardViewer(this.Handle);
+    }
+
+    internal void OnHandleDestroyed(object sender, EventArgs e)
+    {
+      // ビューアを解除
+      bool sts = ChangeClipboardChain(this.Handle, nextHandle);
+      ReleaseHandle();
+    }
+
+    protected override void WndProc(ref Message msg)
+    {
+      switch (msg.Msg) {
+
+        case WM_DRAWCLIPBOARD:
+          if (Clipboard.ContainsText()) {
+            // クリップボードの内容がテキストの場合のみ
+            if (ClipboardHandler != null) {
+              // クリップボードの内容を取得してハンドラを呼び出す
+              ClipboardHandler(this,
+                  new ClipboardEventArgs(Clipboard.GetText()));
+            }
+          }
+          if ((int)nextHandle != 0)
+            SendMessage(
+                nextHandle, msg.Msg, msg.WParam, msg.LParam);
+          break;
+
+        // クリップボード・ビューア・チェーンが更新された
+        case WM_CHANGECBCHAIN:
+          if (msg.WParam == nextHandle) {
+            nextHandle = (IntPtr)msg.LParam;
+          } else if((int)nextHandle != 0)
+            SendMessage(
+                nextHandle, msg.Msg, msg.WParam, msg.LParam);
+          break;
+      }
+      base.WndProc(ref msg);
+    }
+  }
+}
